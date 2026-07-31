2a2da195aa7deabfae1ca6e622164e2cf8637aa6
newuser-xyz <annu031980@gmail.com>
2026-07-31 21:57:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from os import environ
 API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-
-CREDIT = environ.get("CREDIT", "『ᴀᴅᴍɪɴ』")
+OWNER =int(environ.get("OWNER" , "1193248592") )
+CREDIT = environ.get("CREDIT", "𓍯𝙤𝙜𝙗𝙝𝙖𝙩𝙞𝙬𝙖𝙡⚝")
